2f9ab7aaf41e52889effd8b2d07ff0f30bbbf2ae
Giovannimb87 <>
2021-05-20 18:02:02 +0000

    Changed variable candidateName to a string with the value "Holder name"

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ let theLine = "------------------------------------------------";
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "Holder Name";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
@@ -11,7 +11,6 @@ let candidateAnswer = "Sally Ride";
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? ",];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -74,6 +73,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
