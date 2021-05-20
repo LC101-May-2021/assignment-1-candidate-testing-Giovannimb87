7bdb1cec86b84bbb246ecd25b2139b155a8db748
Giovannimb87 <>
2021-05-20 16:10:28 +0000

    Assignment attempt.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,26 +1,39 @@
 const input = require('readline-sync');
-
+let theLine = "------------------------------------------------";
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "1) Who was the first American woman in space? ",
+  "2) True or false: 5 kilometer == 5000 meters? ",
+  "3) (5 + 3)/2 * 10 = ? ",
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "5) What is the minimum crew size for the ISS? ",
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Please, tell me your name. ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+    console.log(theLine);
+    console.log(`>>> Your answer: ${candidateAnswers[i]}`);
+    console.log(`>>> Correct answer: ${correctAnswers[i]}`);
+    console.log(theLine);
+    console.log("");
+  }
 
 }
 
@@ -29,16 +42,46 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
-  
+  let grade = 0;
 
+  for (let i = 0; i < 5; i++) {
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+      grade++;
+    } else {
+      continue;
+    }
+  }
+  grade *= 20;
+  if (grade > 79) {
+    console.log(theLine);
+    console.log(
+      `Congratulations, code-cadet! You passed with a score of ${grade}!`
+    );
+    console.log(theLine);
+  } else {
+    console.log(theLine);
+    console.log(
+      `With a score of ${grade}, You didn't make the cut! Better luck next time.`
+    );
+    console.log(theLine);
+  }
+  console.log(grade);
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.clear();
+  console.log(
+    `Hello, ${candidateName}.\nWelcome, and thank you for participating in the launch_code astronaut application process.`
+  );
+
+  console.log("\n \n");
+  console.log(theLine);
+  console.log("Applicant Name: " + candidateName);
+  console.log(theLine);
+  console.log("\n \n");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
